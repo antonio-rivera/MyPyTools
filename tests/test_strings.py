e3320971bf624ba9130468903cbd582e7ef368f0
Antonio <antoniorivera.cs18@gmail.com>
2026-02-04 15:08:07 -0400

    main.py does not need versioning. Added a safe truncation method and tested it.

--- tests/test_strings.py
+++ tests/test_strings.py
@@ -11,3 +11,21 @@ class TestStrings:
         r1 = strings.normalize_whitespace(teststr1)
         match = re.search(r"\s{2,}", r1)
         assert match == None
+
+        teststr2 = "      "
+        r2 = strings.normalize_whitespace(teststr2)
+        assert r2 == ""
+
+        r3 = strings.normalize_whitespace("")
+        assert r3 == ""
+
+    def test_safe_truncate(self):
+        teststr = "This is my test string for testing truncation."
+        r1 = strings.safe_truncate(teststr, 6)
+        assert r1 == "This is"
+
+        r2 = strings.safe_truncate(teststr, 0)
+        assert r2 == ""
+
+        r3 = strings.safe_truncate(teststr, len(teststr))
+        assert r3 == teststr
--- utils/strings.py
+++ utils/strings.py
@@ -1,2 +1,20 @@
+import re
+
+
 def normalize_whitespace(string: str) -> str:
     return " ".join(string.split())
+
+
+def safe_truncate(string: str, n: int) -> str:
+    truncated = string[0: n]
+
+    matches_original = re.finditer(r"\w+", string)
+    matches_trunc = re.finditer(r"\w+", truncated)
+
+    for (m_trunc, m_org) in zip(matches_trunc, matches_original):
+        if m_trunc.group() != m_org.group():
+            mut_string = list(truncated)
+            mut_string[m_trunc.start():m_trunc.end()] = m_org.group()
+            return "".join(mut_string).strip()
+
+    return truncated.strip()
